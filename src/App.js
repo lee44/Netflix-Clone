fabad2bd2336c26be35ddab42a3a555986951d5b
Lee44 <jlee7772@yahoo.com>
2021-07-03 14:35:23 -0700

    First upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./css/App.css";
+import Row from "./components/Row";
+import requests from "./utils/requests";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        isLargeRow
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
